54930d45b330c57a2ed911479b85497c05f4cba6
Soknitha <hem.sinath@gmail.com>
2026-08-24 18:30:25 +0700

    fix(facebook-pacing): Reduce Facebook post pacing interval to 3 minutes for faster social publishing

--- facebook_publisher.py
+++ facebook_publisher.py
@@ -21,7 +21,7 @@ class FacebookPublisher:
         self.access_token = access_token
         self.graph_url = f"https://graph.facebook.com/v19.0/{self.page_id}"
         self.last_post_time = 0
-        self.min_post_interval = 1800  # 30 mins safety window
+        self.min_post_interval = 180  # 3 mins fast pacing safety window
         self.queue = asyncio.Queue()
         self.worker_task = None
 
